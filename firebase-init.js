2ccfa837119eb679503f039f69c783133698b94b
rkritzar39 <rkritzar39@gmail.com>
2025-10-18 09:42:25 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCIZ0fri5V1E2si1xXpBPQQJqj1F_KuuG0", // Your API key
     authDomain: "busarmydudewebsite.firebaseapp.com",
     projectId: "busarmydudewebsite",
-    storageBucket: "busarmydudewebsite.firebasestorage.app",
+    storageBucket: "busarmydudewebsite.appspot.com",
     messagingSenderId: "42980404680",
     appId: "1:42980404680:web:f4f1e54789902a4295e4fd",
     measurementId: "G-DQPH8YL789" // Optional
