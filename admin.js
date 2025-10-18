1a04bcae92c0e65f170d4e759566c68d23124d51
rkritzar39 <rkritzar39@gmail.com>
2025-10-18 09:33:03 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -2141,7 +2141,7 @@ function formatTimeForPreview(timeString) { // Converts HH:MM to AM/PM format
 onAuthStateChanged(auth, user => {
     // --- User is signed IN ---
     if (user) {
-        const adminEmails = ["ckritzar53@busarmydude.org", "calebkritzar@gmail.com"]; // Your authorized email
+        const adminEmails = ["ckritzar53@busarmydude.org", "rkritzar53@gmail.com"]; // Your authorized email
 
         // Check if the signed-in user is on the admin list
         if (adminEmails.includes(user.email)) {
