7c48e6131ae35274493100df3ea527dd0e06dc82
rkritzar39 <rkritzar39@gmail.com>
2026-06-15 22:36:31 -0400

    Update displayShoutouts.js

--- displayShoutouts.js
+++ displayShoutouts.js
@@ -974,20 +974,6 @@ async function fetchLatestOSVersions() {
 }
 
 // ======================
-// SAFE HTML
-// ======================
-function escapeHTML(value) {
-    if (value === null || value === undefined) return "";
-
-    return String(value)
-        .replaceAll("&", "&amp;")
-        .replaceAll("<", "&lt;")
-        .replaceAll(">", "&gt;")
-        .replaceAll('"', "&quot;")
-        .replaceAll("'", "&#039;");
-}
-
-// ======================
 // OS TYPE DETECTION
 // ======================
 function detectOSType(osVersion) {
